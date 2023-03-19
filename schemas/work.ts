11ed9811bae185c70ec09fac307441786301d81b
Arth K. Gajjar <iarthstar@gmail.com>
2023-03-19 17:55:58 +0530

    Added  : init work schema

---
+++ schemas/work.ts
@@ -0,0 +1,12 @@
+export default {
+    name: 'work',
+    type: 'document',
+      title: 'Work',
+    fields: [
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Name'
+      }
+    ]
+  }
